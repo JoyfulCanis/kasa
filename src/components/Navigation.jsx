c162ba15fc0dd904e8668cbcc8edcfc8ec4a5321
Neïl Aloulou <98472708+JoyfulCanis@users.noreply.github.com>
2023-05-25 16:33:52 +0200

    changed 'Navlink / li' order. li now includes Navlink

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,13 +7,16 @@ const Navigation = () => {
     return (
         <nav className='header__navigation'>
             <ul className='header__navigation__list'>
-                <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
-                    <li>Accueil</li>
-                </NavLink>
-
-                <NavLink to="/About" className={(nav) => (nav.isActive ? "nav-active" : "")}>
-                    <li>A Propos</li>
-                </NavLink>
+                <li>
+                    <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
+                        Accueil
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to="/About" className={(nav) => (nav.isActive ? "nav-active" : "")}>
+                        A Propos
+                    </NavLink>
+                </li>
 
             </ul>
 
